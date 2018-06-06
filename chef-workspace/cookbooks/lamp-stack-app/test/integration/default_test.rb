76f5af58d2d57405ba2d1a2c003c0d2650cca1d1
Fábio Souza <fabio.souza.eti@gmail.com>
2018-06-06 02:29:45 -0300

    Added custom workaround to allow converge e run tests in multiple nodes

--- chef-workspace/cookbooks/lamp-stack-app/test/integration/default_test.rb
+++ chef-workspace/cookbooks/lamp-stack-app/test/integration/default_test.rb
@@ -7,12 +7,31 @@
 
 unless os.windows?
   # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
+  control "User root should be created" do
+    title 'root exist'
+    describe user('root') do
+      it { should exist }
+    end
   end
 end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+control "Port 80 should be listening" do
+  title 'port is listening'
+  describe port(80)  do
+    it { should be_listening }
+  end
 end
+
+#match 
+2.times do |n|
+  control "Checking Web instance #{n+1}" do
+    title "Web instance #{n+1} is ok"
+    describe command('curl localhost') do
+      #check balancer 1
+      its(:stdout) { should match(/app-web[1-2]/)}
+    end
+  end 
+end  
+
+#TODO add a test to verify connection of application with database
